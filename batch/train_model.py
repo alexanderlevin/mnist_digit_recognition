057de3123eeaf488c00c46030c4d8d58568aeca2
Alex Levin <alexanderlevin@users.noreply.github.com>
2022-08-20 19:14:12 -0700

    Modernize implementation of the model
    - Use keras
    - Load dataset using tensorflow_datasets
    - Add a requirements file

--- batch/train_model.py
+++ batch/train_model.py
@@ -1,88 +1,56 @@
 import argparse
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow_datasets as tfds
 
 parser = argparse.ArgumentParser(description="Batch arguments")
-parser.add_argument('--num-training-steps', type=int, dest='num_training_steps', default=2000)
-parser.add_argument('--mini-batch-size', type=int, dest='mini_batch_size', default=50)
+parser.add_argument('--num-epochs', type=int, dest='num_epochs', default=5)
+parser.add_argument('--batch-size', type=int, dest='batch_size', default=128)
 args = parser.parse_args()
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
 
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+def normalize_img(image, label):
+    return tf.cast(image, tf.float32) / 255., label
 
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+def extract_and_preprocess_dataset(batch_size: int):
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+
+    ds_train = ds_train.map(
+        normalize_img, num_parallel_calls=tf.data.AUTOTUNE
+    )
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits["train"].num_examples)
+    ds_train = ds_train.batch(batch_size)
+    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+    return ds_train
 
 
 def run():
-    sess = tf.Session()
+    ds_train = extract_and_preprocess_dataset(args.batch_size)
 
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Resizing(28, 28),
+            tf.keras.layers.Conv2D(32, 5, padding="SAME"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(64, 5, padding="SAME"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Reshape((7 * 7 * 64,)),
+            tf.keras.layers.Dense(1024),
+            tf.keras.layers.Dense(10)
+        ]
+    )
 
-    x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
-    y_ = tf.placeholder(tf.float32, shape=[None, 10])
-
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+    model.compile(optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+    model.fit(ds_train, epochs=args.num_epochs)
+    model.save("saved_models")
 
 
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
-
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
-
-    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name="y_conv")
-
-    cross_entropy = tf.reduce_mean(
-        -tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    sess.run(tf.initialize_all_variables())
-    for i in xrange(args.num_training_steps):
-        batch = mnist.train.next_batch(args.mini_batch_size)
-        if i % 100 == 0:
-            train_accuracy = accuracy.eval(
-                feed_dict={
-                    x:batch[0], y_: batch[1], keep_prob: 1.0},
-                session=sess)
-            print "step %d, training accuracy %g" %(i, train_accuracy)
-        train_step.run(session=sess,
-            feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-    print "test accuracy %s"%accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},
-        session=sess)
-
-    saver = tf.train.Saver()
-    save_path = saver.save(sess, 'model.ckpt')
-    print "Model saved in file {}".format(save_path)
-
-if __name__=='__main__':
+if __name__ == '__main__':
     run()
